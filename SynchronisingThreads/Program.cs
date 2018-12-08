c9184ddccd1284aa6cbf45538727c537f8a1ad13
Amir Begic <amir.begic@namics.com>
2018-12-08 17:47:26 +0100

    Task 2: Implemented file comparison with char arrays. Two command line arguments need to be provided for the program to function.

--- SynchronisingThreads/Program.cs
+++ SynchronisingThreads/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
@@ -11,34 +12,55 @@ namespace SynchronisingThreads
 {
     class Program
     {
-        private static char ch;
+        private static char[] charArray1 = new char[1000];
+        private static char[] charArray2 = new char[1000];
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Press a Key to change the recurring character.");
-            ch = 'A';
-            var printer = new Thread(new ThreadStart(PrintCh));
-            var reader = new Thread(new ThreadStart(ReadKey));
 
-            printer.Start();
-            reader.Start();
-            
-        }
-
-        private static void PrintCh()
-        {
-            while (true)
+            if (args[1] == null || args[1] == "")
             {
-                Console.Write(ch);
-                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Please provide two filepaths as parameters. Exiting Application...");
+                Console.ReadKey();
+                System.Environment.Exit(1);
             }
+
+            Console.WriteLine("Comparing the provided files");
+            var t1 = new Thread(new ThreadStart(delegate {ReadFileToCharArray(charArray1, args[0]);}));
+            var t2 = new Thread(new ThreadStart(delegate {ReadFileToCharArray(charArray2, args[1]);}));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                if (!charArray1[i].Equals(charArray2[i]))
+                {
+                    Console.WriteLine("The Files are not the same. Exiting Program.");
+                    Console.ReadKey();
+                    System.Environment.Exit(1);
+                }
+                
+            }
+
+            Console.WriteLine("The Files are identical. Exiting Program.");
+            Console.ReadKey();
+
         }
 
-        private static void ReadKey()
+        public static void ReadFileToCharArray(char[] chArr, string filepath)
         {
-            while (true)
+            using (StreamReader sr = new StreamReader(filepath))
             {
-                ch = Console.ReadKey().KeyChar;
+                int i = 0;
+                while (!sr.EndOfStream)
+                {
+                    chArr[i] = (char)sr.Read();
+                    i++;
+                }
             }
         }
 
