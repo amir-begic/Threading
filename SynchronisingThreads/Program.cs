0c9b10beb068a6b4e24619cf62b228a7a2d80a01
Amir Begic <amir.begic@namics.com>
2019-01-08 18:45:17 +0100

    Task 4: fixed Output.

--- SynchronisingThreads/Program.cs
+++ SynchronisingThreads/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SynchronisingThreads
 {
@@ -14,7 +8,7 @@ namespace SynchronisingThreads
     {
         public class Philosopher
         {
-            private int timeToEat = 2000;
+            private int timeToEat = 10000;
             private int timeToThink = 3000;
             private int _philsophersNumber;
             private Fork _leftFork;
@@ -30,21 +24,21 @@ namespace SynchronisingThreads
             {
                 lock (_leftFork)
                 {
-                    Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork);
+                    Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork._forkNumber);
 
                     lock (_rightFork)
                     {
                         // Eat here 
-                        Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork);
+                        Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork._forkNumber);
                         Console.WriteLine("Philosopher {0} is eating.", _philsophersNumber);
 
                         Thread.Sleep(timeToEat); 
                     }
 
-                    Console.WriteLine("   Philosopher {0} released {1} fork.", _philsophersNumber, _rightFork);
+                    Console.WriteLine("   Philosopher {0} released {1} fork.", _philsophersNumber, _rightFork._forkNumber);
                 }
                 
-                Console.WriteLine("Philosopher {0} released {1} fork.", _philsophersNumber, _leftFork);
+                Console.WriteLine("Philosopher {0} released {1} fork.", _philsophersNumber, _leftFork._forkNumber);
                 this.Think();
             }
 
@@ -58,7 +52,7 @@ namespace SynchronisingThreads
 
         public class Fork
         {
-            private int _forkNumber;
+            public int _forkNumber;
             public Fork(int forkNumber)
             {
                 _forkNumber = forkNumber;
