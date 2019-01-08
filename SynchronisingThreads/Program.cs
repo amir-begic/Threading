aea0f0a894eefb8ca51c3a248c676ddb5741dcc4
Amir Begic <amir.begic@namics.com>
2019-01-08 18:39:12 +0100

    Task 4: Implement the Philosopher's Problem with Threads in C#

--- SynchronisingThreads/Program.cs
+++ SynchronisingThreads/Program.cs
@@ -12,57 +12,89 @@ namespace SynchronisingThreads
 {
     class Program
     {
-        private static char[] charArray1 = new char[1000];
-        private static char[] charArray2 = new char[1000];
-
-        static void Main(string[] args)
+        public class Philosopher
         {
-
-            if (args[1] == null || args[1] == "")
+            private int timeToEat = 2000;
+            private int timeToThink = 3000;
+            private int _philsophersNumber;
+            private Fork _leftFork;
+            private Fork _rightFork;
+            public Philosopher(int philsophersNumber, Fork leftFork, Fork rightFork)
             {
-                Console.WriteLine("Please provide two filepaths as parameters. Exiting Application...");
-                Console.ReadKey();
-                System.Environment.Exit(1);
+                _philsophersNumber = philsophersNumber;
+                _leftFork = leftFork;
+                _rightFork = rightFork;
             }
 
-            Console.WriteLine("Comparing the provided files");
-            var t1 = new Thread(new ThreadStart(delegate {ReadFileToCharArray(charArray1, args[0]);}));
-            var t2 = new Thread(new ThreadStart(delegate {ReadFileToCharArray(charArray2, args[1]);}));
-
-            t1.Start();
-            t2.Start();
-
-            t1.Join();
-            t2.Join();
-
-            for (int i = 0; i < charArray1.Length; i++)
+            public void Eat()
             {
-                if (!charArray1[i].Equals(charArray2[i]))
+                lock (_leftFork)
                 {
-                    Console.WriteLine("The Files are not the same. Exiting Program.");
-                    Console.ReadKey();
-                    System.Environment.Exit(1);
+                    Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork);
+
+                    lock (_rightFork)
+                    {
+                        // Eat here 
+                        Console.WriteLine("Philosopher {0} picked up Fork {1}.", _philsophersNumber, _leftFork);
+                        Console.WriteLine("Philosopher {0} is eating.", _philsophersNumber);
+
+                        Thread.Sleep(timeToEat); 
+                    }
+
+                    Console.WriteLine("   Philosopher {0} released {1} fork.", _philsophersNumber, _rightFork);
                 }
                 
+                Console.WriteLine("Philosopher {0} released {1} fork.", _philsophersNumber, _leftFork);
+                this.Think();
             }
 
-            Console.WriteLine("The Files are identical. Exiting Program.");
-            Console.ReadKey();
-
-        }
-
-        public static void ReadFileToCharArray(char[] chArr, string filepath)
-        {
-            using (StreamReader sr = new StreamReader(filepath))
+            public void Think()
             {
-                int i = 0;
-                while (!sr.EndOfStream)
-                {
-                    chArr[i] = (char)sr.Read();
-                    i++;
-                }
+                Console.WriteLine("Philosopher {0} is thinking", _philsophersNumber);
+                Thread.Sleep(timeToThink);
+                Eat();
             }
         }
+
+        public class Fork
+        {
+            private int _forkNumber;
+            public Fork(int forkNumber)
+            {
+                _forkNumber = forkNumber;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var count = 5;
+            var philosophers = new List<Philosopher>();
+            var forks = new List<Fork>();
+
+            //create forks
+            for (int i = 0; i < 5; i++)
+            {
+                forks.Add(new Fork(i));
+            }
+
+            //create philosophers and assign forks
+            int fi = 1;
+            foreach (Fork f in forks)
+            {
+                philosophers.Add(new Philosopher(fi, forks[fi - 1 == 0 ? 4 : fi - 1 ], forks[fi == 5 ? 1 : fi]));
+                fi++;
+            }
+
+            //create and run threads
+            foreach (Philosopher p in philosophers)
+            {
+                new Thread(() =>
+                {
+                    p.Eat();
+                }).Start();
+            }
+
+        }
+     
 
     }
 }
